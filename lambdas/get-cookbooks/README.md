769e647fa0077be6ee3392c15746cb257237b08f
Nancy Prestridge <nancy.the.dev@gmail.com>
2017-03-26 19:28:50 -0400

    chore: Remove lambdas

--- lambdas/get-cookbooks/README.md
+++
@@ -1,4 +0,0 @@
-# Local Development
-`npm install`
-To run node scripts locally: `sudo npm install -g babel-cli`
-To run mock script: `npm run-script mock`
--- lambdas/get-recipes-by-cookbook/src/config/production.js
+++
@@ -1,7 +0,0 @@
-module.exports.values = {
-  dynamodb: {
-    apiVersion: '2012-08-10',
-    region: 'us-east-1',
-    endpoint: 'dynamodb.us-east-1.amazonaws.com',
-  }
-};
