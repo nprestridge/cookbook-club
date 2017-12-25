885ae3eb99a65d7f262997c5158751e318ccad30
nprestridge <nancy@Nancys-Air.home>
2017-12-25 16:13:12 -0500

    feat: Add footer

--- src/view/Index.js
+++ src/view/Index.js
@@ -4,9 +4,13 @@ import '../styles/index.css';
 class Index extends Component {
   render() {
     const { main } = this.props;
+
+    const d = new Date();
+    const year = d.getFullYear();
+
     return (
       <div>
-        <header className="header">
+        <header>
           <a href="/">
             <img src="/images/logo_banner.png" alt="Cookbook Club" height="100" width="300" />
           </a>
@@ -15,6 +19,10 @@ class Index extends Component {
         <main className="container">
           {main}
         </main>
+
+        <footer className="footer footer--text">
+          &copy; {year} Nancy&apos;s Hearth.  All rights reserved.
+        </footer>
       </div>
     );
   }
