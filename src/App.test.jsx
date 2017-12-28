67f56c5e8668c181278077c68a7bf9e865088efe
nprestridge <nprestridge@users.noreply.github.com>
2017-12-27 20:49:32 -0500

    feat: Add fetch-mock for mocking unit test API calls

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FetchMock from 'fetch-mock';
 import App from './App';
 
 it('renders without crashing', () => {
+  const cookbooks = [
+    {
+      title: 'Cookbook One',
+      author: 'Author One',
+    },
+    {
+      title: 'Cookbook Two',
+      author: 'Author Two',
+    },
+  ];
+
+  // Mock fetch
+  FetchMock.get('*', cookbooks);
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+
+  // Unmock
+  FetchMock.restore();
 });
