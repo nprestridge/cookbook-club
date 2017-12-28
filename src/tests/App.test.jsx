d1a4cc217ced6bda56755cea724b981169a8b49c
nprestridge <nprestridge@users.noreply.github.com>
2017-12-28 06:35:39 -0500

    chore: Move tests to tests folder.  Update script names to consistent format.

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FetchMock from 'fetch-mock';
-import App from './App';
+import App from '../App';
 
 it('renders without crashing', () => {
   const cookbooks = [
