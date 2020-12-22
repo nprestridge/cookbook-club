5c6fde5884ab947ecd6a6fb5673c6671736e72cc
nprestridge <nprestridge@users.noreply.github.com>
2020-12-21 20:54:57 -0500

    feat: Add navigation menu

--- src/view/Layout.jsx
+++ src/view/Layout.jsx
@@ -1,33 +1,84 @@
+import { Link } from 'react-router';
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/index.css';
 
-const Layout = (props) => {
-  const { main } = props;
+class Layout extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    const { main } = props;
 
-  const d = new Date();
-  const year = d.getFullYear();
+    this.state = {
+      main,
+      showNav: '',
+    };
+  }
 
-  return (
-    <div>
-      <header>
-        <a href="/">
-          <img src="/images/logo_banner_w300.png" alt="Cookbook Club" height="100" width="300" />
-        </a>
-      </header>
+  componentDidUpdate(prevProps) {
+    this.updateMainComponent(prevProps.main);
+  }
 
-      <main className="container">
-        {main}
-      </main>
+  updateMainComponent(prevMain) {
+    const { main } = this.props;
+    if (prevMain !== main) {
+      this.setState({
+        main,
+        showNav: '',
+      });
+    }
+  }
 
-      <footer className="page-footer">
-        &copy;&nbsp;
-        {year}
-        &nbsp;Nancy&apos;s Hearth.  All rights reserved.
-      </footer>
-    </div>
-  );
-};
+  toggleNav() {
+    const {
+      showNav,
+    } = this.state;
+
+    this.setState({
+      showNav: (showNav === '') ? 'nav__toggle--active' : '',
+    });
+  }
+
+  render() {
+    const {
+      showNav,
+      main,
+    } = this.state;
+
+    const d = new Date();
+    const year = d.getFullYear();
+
+    return (
+      <div>
+        <header>
+          <nav>
+            <span className="nav__toggle" role="presentation" onClick={this.toggleNav.bind(this)} onKeyDown={this.toggleNav.bind(this)}>
+              <i className="fas fa-bars fa-sm" />
+            </span>
+            <div className="logo">
+              <Link to="/">
+                <img src="/images/logo_banner_w300.png" alt="Cookbook Club" height="100" width="300" />
+              </Link>
+            </div>
+            <ul className={showNav}>
+              <li><Link to="/">Cookbooks</Link></li>
+              <li><Link to="/recipes">Recipes</Link></li>
+            </ul>
+          </nav>
+        </header>
+
+        <main className="container">
+          {main}
+        </main>
+
+        <footer className="page-footer">
+          &copy;&nbsp;
+          {year}
+          &nbsp;Nancy&apos;s Hearth.  All rights reserved.
+        </footer>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   main: PropTypes.element.isRequired,
