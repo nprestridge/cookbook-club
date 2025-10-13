82d0e7d23d5100cf106840a95f58c9189c531c32
nprestridge <nprestridge@users.noreply.github.com>
2025-10-13 17:50:55 -0400

    feat: Change urls to use slugs - Create cookbook store

---
+++ src/controller/CookbookStore.js
@@ -0,0 +1,13 @@
+let cookbooks = [];
+
+const CookbookStore = {
+  setCookbooks(books) {
+    cookbooks = books;
+  },
+
+  getCookbookBySlug(slug) {
+    return cookbooks.find(book => book.slug === slug);
+  },
+};
+
+export default CookbookStore;
