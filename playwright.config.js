0d708e5e7e4042574af1865d7491c7754eac1d52
nprestridge <nprestridge@users.noreply.github.com>
2026-05-10 18:07:48 -0400

    Improve test performance

--- playwright.config.js
+++ playwright.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
@@ -13,7 +13,9 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
 
-  projects: [
+  projects: process.env.CI ? [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  ] : [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
     { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
     { name: 'webkit', use: { ...devices['Desktop Safari'] } },
@@ -22,6 +24,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
